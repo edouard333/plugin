52ef66be6f0e5eb0fdc1d5070562c6cdb9a2a54b
GitHub Action <action@github.com>
2022-07-06 21:26:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/edouard333/plugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/edouard333/plugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.edouard333.plugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.edouard333.plugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/edouard333/plugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/edouard333/plugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.edouard333.plugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.edouard333.plugin.MyBundle
 
 class MyProjectService(project: Project) {
 
